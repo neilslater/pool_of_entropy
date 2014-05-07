ae2b7eceee6efda8dd2fb1bdc4457a69d3a41644
Neil Slater <neil@aframe.com>
2014-05-07 11:46:49 +0100

    More tests

--- lib/pool_of_entropy/core_prng.rb
+++ lib/pool_of_entropy/core_prng.rb
@@ -1,12 +1,39 @@
 require 'digest/sha2'
 require 'securerandom'
 
+# This class implements a random number generator based on SHA-512
+#
+# An object of the class has internal state that is modified on each call to
+# #update or any #read_... method (internally the #read_... methods call #update).
+# The #read_... methods generate a pseudo-random number from the current
+# state pool using SHA-512 and use it in the return value. It is not feasible to
+# determine the internal state from the pseudo-random data received, and not
+# possible to manipulate results from the PRNG in a predictable manner without
+# knowing the internal state.
+#
+# @example Using default internal state, initialised using SecureRandom.random_bytes
+#  prng = PoolOfEntropy::CorePRNG.new
+#  prng.read_bytes
+#  # E.g. => "]\x12\x9E\xF5\x17\xF3\xC2\x1A\x15\xDFu]\x95\nd\x12"
+#  prng.read_hex
+#  # E.g. => "a0e00d2848242ec49e0a15ef411ba647"
+#  prng.read_bignum
+#  # E.g. => 33857278877368906880463811096418580004
+#  prng.read_float
+#  # E.g. => 0.6619838265836278
+#
+
 class PoolOfEntropy::CorePRNG
 
-  def initialize size = 1, initial_state = SecureRandom.random_bytes( 64 * size ), mix_block_id = 0
+  # Creates a new random number source. All parameters are optional.
+  # @param [Integer] size Number of 64-byte blocks, between 1 and 256 (16KB)
+  # @param [String] initial_state Sets contents of state pool (default uses SecureRandom)
+  # @param [Integer] mix_block_id
+  # @return [PoolOfEntropy::CorePRNG]
+  def initialize size = 1, initial_state = SecureRandom.random_bytes( 64 * Integer(size) ), mix_block_id = 0
     @size = Integer( size )
-    if @size < 1 || @size > 64
-      raise ArgumentError, "Size of pool must be in Range 1..64, got #{@size}"
+    if @size < 1 || @size > 256
+      raise ArgumentError, "Size of pool must be in Range 1..256, got #{@size}"
     end
     unless initial_state.is_a? String
       raise TypeError, "Initial state must be a String, got #{initial_state.inspect}"
@@ -15,26 +42,38 @@ class PoolOfEntropy::CorePRNG
     @state.force_encoding( 'BINARY' )
 
     if @state.size != size * 64
-      raise ArgumentError, "Initial state wrong - expected #{size * 64} bytes, got #{@state.size} bytes"
+      raise ArgumentError, "Initial state bad size - expected #{size * 64} bytes, got #{@state.size} bytes"
     end
 
     @mix_block_id = Integer( mix_block_id ) % @size
   end
 
+  # The number of 64-byte blocks used in the internal state pool.
+  # @return [Integer]
   attr_reader :size
 
+  # Identifies the next 64-byte block in the pool that will be altered
+  # by a read or update process.
+  # @return [Integer]
   attr_reader :mix_block_id
 
+  # A clone of the internal state pool. In combination with #mix_block_id, describes
+  # the whole PRNG. If this value is supplied to an end user, then they can easily
+  # predict future values of the PRNG.
+  # @return [PoolOfEntropy::CorePRNG]
   def state
     @state.clone
   end
 
+  # The clone of a PoolOfEntropy::CorePRNG object includes separate copy of internal state
+  # @return [PoolOfEntropy::CorePRNG]
   def clone
     PoolOfEntropy::CorePRNG.new( self.size, self.state, self.mix_block_id )
   end
 
-  # Mixes supplied data into the curent state
-  # @param [String] data adjustments
+  # Mixes supplied data into the curent state. This is called
+  # internally by #read_... methods as well.
+  # @param [String] data Data to be mixed. Note empty string '' and nil are equivalent and *do* change the state.
   # @return [nil]
   def update data
     new_block = Digest::SHA512.digest( @state + data.to_s )
@@ -44,7 +83,7 @@ class PoolOfEntropy::CorePRNG
   end
 
   # Statistically flat distribution of 128 bits (16 bytes)
-  # @param [Array<String>] adjustments
+  # @param [Array<String>] adjustments mixed in using SHA-512, so that they affect return value, but not internal state
   # @return [String] 16 characters in ASCII-8BIT encoding
   def read_bytes *adjustments
     raw_digest = Digest::SHA512.digest( @state )
@@ -55,31 +94,41 @@ class PoolOfEntropy::CorePRNG
     fold_bits( fold_bits( raw_digest ) )
   end
 
+  # Statistically flat distribution of 32 hex digits
+  # @param [Array<String>] adjustments mixed in using SHA-512, so that they affect return value, but not internal state
+  # @return [String] 32 hex digits
+  def read_hex *adjustments
+    read_bytes( *adjustments ).unpack('H*').first
+  end
+
   # Statistically flat distribution from range 0...2**128
-  # @param [Array<String>] adjustments
-  # @return [Fixnum,Bignum] between 0 and 0xffffffffffffffffffffffffffffffff
+  # @param [Array<String>] adjustments mixed in using SHA-512, so that they affect return value, but not internal state
+  # @return [Bignum,Fixnum] between 0 and 0xffffffffffffffffffffffffffffffff
   def read_bignum *adjustments
     nums = read_bytes( *adjustments ).unpack('Q>*')
     nums.inject(0) { |sum,v| (sum << 64) + v }
   end
 
   # Statistically flat distribution from interval 0.0...1.0, with 53-bit precision
-  # @param [Array<String>] adjustments
+  # @param [Array<String>] adjustments mixed in using SHA-512, so that they affect return value, but not internal state
   # @return [Float] between 0.0 and 0.9999999999999999
   def read_float *adjustments
-    num = read_bytes( *adjustments ).unpack('Q>*').first << 11
+    num = read_bytes( *adjustments ).unpack('Q>*').first >> 11
     num.to_f / 2 ** 53
   end
 
   private
 
-  # NB only works for messages which are multiples of 8 bytes long
+  # Xors first half of a message with second half. Only works for messages
+  # which are multiples of 8 bytes long.
+  # @param [String] msg bytes to fold
+  # @return [String] folded message, half the length of original
   def fold_bits msg
     l = msg.length/2
-    numeric_return = msg[0,l].unpack('L>*').zip( msg[l,l].unpack('L>*') ).map do |x,y|
+    folded_32bits = msg[0,l].unpack('L>*').zip( msg[l,l].unpack('L>*') ).map do |x,y|
       x ^ y
     end
-    numeric_return.pack('L>*')
+    folded_32bits.pack('L>*')
   end
 
 end
--- spec/core_prng_spec.rb
+++ spec/core_prng_spec.rb
@@ -1,11 +1,161 @@
 require 'pool_of_entropy'
+require 'set'
 
 describe PoolOfEntropy::CorePRNG do
   describe "class methods" do
 
     describe "#new" do
       it "should instantiate a default object" do
-        PoolOfEntropy::CorePRNG.new.should be_a PoolOfEntropy::CorePRNG
+        prng = PoolOfEntropy::CorePRNG.new
+        prng.should be_a PoolOfEntropy::CorePRNG
+        prng.size.should == 1
+      end
+
+      it "should allow setting number of blocks in pool" do
+        prng = PoolOfEntropy::CorePRNG.new( 10 )
+        prng.should be_a PoolOfEntropy::CorePRNG
+        prng.size.should == 10
+      end
+
+      it "should fail with incorrect block number" do
+        expect { PoolOfEntropy::CorePRNG.new( -43 ) }.to raise_error ArgumentError
+        expect { PoolOfEntropy::CorePRNG.new( -1 ) }.to raise_error ArgumentError
+        expect { PoolOfEntropy::CorePRNG.new( 0 ) }.to raise_error ArgumentError
+        expect { PoolOfEntropy::CorePRNG.new( 257 ) }.to raise_error ArgumentError
+        expect { PoolOfEntropy::CorePRNG.new( 1000) }.to raise_error ArgumentError
+        expect { PoolOfEntropy::CorePRNG.new( nil ) }.to raise_error TypeError
+        expect { PoolOfEntropy::CorePRNG.new( '' ) }.to raise_error ArgumentError
+        expect { PoolOfEntropy::CorePRNG.new( :foo => 2 ) }.to raise_error TypeError
+      end
+
+      it "should allow setting internal state" do
+        prng = PoolOfEntropy::CorePRNG.new( 1, "\x0" * 64 )
+        prng.should be_a PoolOfEntropy::CorePRNG
+        prng.size.should == 1
+        prng.state.should == "\x0" * 64
+      end
+
+      it "should fail with bad state data" do
+        expect { PoolOfEntropy::CorePRNG.new( 1, '' ) }.to raise_error ArgumentError
+        expect { PoolOfEntropy::CorePRNG.new( 1, "\x0" * 63 ) }.to raise_error ArgumentError
+        expect { PoolOfEntropy::CorePRNG.new( 1, "\x0" * 200 ) }.to raise_error ArgumentError
+        expect { PoolOfEntropy::CorePRNG.new( 2, "\x0" * 64  ) }.to raise_error ArgumentError
+      end
+
+      it "should allow setting mix_block_id" do
+        prng = PoolOfEntropy::CorePRNG.new( 3, "\x12" * 192, 1 )
+        prng.should be_a PoolOfEntropy::CorePRNG
+        prng.size.should == 3
+        prng.state.should == "\x12" * 192
+        prng.mix_block_id.should == 1
+      end
+    end
+  end
+
+  describe "instance methods" do
+
+    describe "#clone" do
+      it "should copy all attributes" do
+        prng_orig = PoolOfEntropy::CorePRNG.new
+        prng_copy = prng_orig.clone
+
+        prng_copy.size.should == prng_orig.size
+        prng_copy.state.should == prng_orig.state
+        prng_copy.mix_block_id.should == prng_orig.mix_block_id
+      end
+
+      it "should deep clone the internal state string" do
+        prng_orig = PoolOfEntropy::CorePRNG.new
+        prng_copy = prng_orig.clone
+        prng_copy.state.should_not be prng_orig.state
+      end
+    end
+
+    describe "#update" do
+      it "should change the internal state" do
+        prng = PoolOfEntropy::CorePRNG.new
+        init_state = prng.state.clone
+        prng.update( 'boo' )
+        prng.state.should_not == init_state
+      end
+
+      it "should not change the length of the internal state" do
+        prng = PoolOfEntropy::CorePRNG.new
+        prng.update( 'boo' )
+        prng.state.length.should == 64
+        prng.update( 'boowgkjwrhqgioueqrhgiue2hguirhqwiughreuioghreuifhqwoifhr3iufghfwrgrwgetdfwd' )
+        prng.state.length.should == 64
+
+        prng = PoolOfEntropy::CorePRNG.new( 5 )
+        prng.update( 'boo' )
+        prng.state.length.should == 5 * 64
+        prng.update( 'getdfwd' * 1000 )
+        prng.state.length.should == 5 * 64
+        prng.update( 'boefewfweo' )
+        prng.state.length.should == 5 * 64
+        prng.update( 'geefewftdfwd' * 1000 )
+        prng.state.length.should == 5 * 64
+      end
+
+      it "should only change 64 bytes of state at a time" do
+        prng = PoolOfEntropy::CorePRNG.new( 5 )
+        init_state = prng.state.clone
+
+        prng.update( 'boo' )
+        prng.state[64,4*64].should == init_state[64,4*64]
+        next_state = prng.state.clone
+
+        prng.update( 'getdfwd' * 1000 )
+        prng.state[128,3*64].should == init_state[128,3*64]
+        prng.state[0,1*64].should == next_state[0,1*64]
+        next_state = prng.state.clone
+
+        prng.update( 'boefewfweo' )
+        prng.state[192,2*64].should == init_state[192,2*64]
+        prng.state[0,2*64].should == next_state[0,2*64]
+        next_state = prng.state.clone
+
+        prng.update( 'geefewftdfwd' * 1000 )
+        prng.state[256,1*64].should == init_state[256,1*64]
+        prng.state[0,3*64].should == next_state[0,3*64]
+      end
+    end
+
+    pool_types = [
+      [
+        'default params',
+        PoolOfEntropy::CorePRNG.new
+      ],
+      [
+        'size of 20',
+        PoolOfEntropy::CorePRNG.new( 20 )
+      ],
+      [
+        'zeroed state',
+        PoolOfEntropy::CorePRNG.new( 1, "\x0" * 64 )
+      ],
+      [
+        'five blocks fixed state and initial mix block 3',
+        PoolOfEntropy::CorePRNG.new( 5, "fiver" * 64, 3 )
+      ],
+    ]
+
+    # NB "probabilty" and "randomness" tests in the following block are very light, just
+    # intended to capture high-level failures in logic.
+    pool_types.each do |prng_name, prng|
+
+      context "using #{prng_name}" do
+
+        describe "#read_bytes" do
+          it "always returns a 16 byte string" do
+            100.times { prng.read_bytes.length.should == 16 }
+          end
+
+          it "has a high probability of returning a different string each time" do
+            Set[ *(1..100).map {prng.read_bytes} ].size.should == 100
+          end
+        end
+
       end
     end
   end
