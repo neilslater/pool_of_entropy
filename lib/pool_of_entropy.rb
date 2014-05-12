c7a053fd9d6a0de431ee26aff2057a837152725d
Neil Slater <neil@aframe.com>
2014-05-12 21:10:47 +0100

    Constructor and a little more doc

--- lib/pool_of_entropy.rb
+++ lib/pool_of_entropy.rb
@@ -12,20 +12,69 @@ require "pool_of_entropy/core_prng"
 #  prng.rand( 20 )
 #  # E.g. => 12
 #
+# @example A customised PRNG, seeded with some user data, using webcam for "true" randomness
+#  prng = PoolOfEntropy.new :size => 4, :blank => true, :seeds = ['Hello!',picture_bytes]
+#  loop do
+#    prng.add_to_pool( Webcam.image.bytes ) # Imagined Webcam interface
+#    prng.rand( 20 )
+#    # E.g. => 12
+#    sleep 5
+#  end
+#
 
 class PoolOfEntropy
 
   # Creates a new random number source. All parameters are optional.
   # @param [Hash] options
-  # @param [String] initial_state Sets contents of state pool (default uses SecureRandom)
-  # @param [Integer] mix_block_id
+  # @option options [Integer] :size, number of 512-bit (64 byte) blocks to use as internal state, defaults to 1
+  # @option options [Boolean] :blank, if true then initial state is all zeroes, otherwise use SecureRandom
+  # @option options [Array<String>] :seeds, if provided these are sent to #add_to_pool during initialize
   # @return [PoolOfEntropy]
-  def initialize options
-    @core_prng = CorePRNG.new( size, initial_state, mix_block_id )
+  def initialize options = {}
+    unless options.is_a? Hash
+      raise TypeError, "Expecting an options hash, got #{options.inspect}"
+    end
+
+    size = 1
+    if options[:size]
+      size = Integer( size )
+      if size < 1 || size > 256
+        raise ArgumentError, "Size of pool must be in Range 1..256, got #{size}"
+      end
+    end
+
+    initial_state = if options[:blank]
+      "\x0" * size * 64
+    else
+      SecureRandom.random_bytes( size * 64 )
+    end
+
+    @core_prng = CorePRNG.new( size, initial_state )
+
+    if options[:seeds]
+      unless options[:seeds].is_a? Array
+        raise TypeError, "Expected value for :seeds to be an Array, got #{options[:seeds].inspect}"
+      end
+      options[:seeds].each do |seed|
+        add_to_pool( seed )
+      end
+    end
+
     @next_modifier_queue = []
     @fixed_modifier = nil
   end
 
+  # Cloning creates a deep copy with identical PRNG state and modifiers
+  # @return [PoolOfEntropy]
+  def clone
+    Marshal.load( Marshal.dump( self ) )
+  end
+
+  # Same functionality as Kernel#rand or Random#rand, but using
+  # current pool state to generate number, and including zero, one or
+  # two modifiers that are in effect.
+  # @param [Integer,Range] max use 0 to return
+  # @return [Fixnum,Bignum,Float] type depends on value of max
   def rand max = 0
     if max.is_a? Range
       bottom, top = max.minmax
@@ -43,8 +92,11 @@ class PoolOfEntropy
 
   def modify_next *modifiers
     modifiers.each do |modifier|
-      next if modifier.nil?
-      @next_modifier_queue << Digest::SHA512.digest( modifier.to_s )
+      if modifier.nil?
+        @next_modifier_queue << nil
+      else
+        @next_modifier_queue << Digest::SHA512.digest( modifier.to_s )
+      end
     end
     self
   end
